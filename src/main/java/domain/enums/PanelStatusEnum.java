f1bd0af94a2bcf6b5b714bac89ab0956f51cf453
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2023-05-06 16:47:14 -0300

    docs: updated logic flux

--- src/main/java/domain/enums/PanelStatusEnum.java
+++ src/main/java/domain/enums/PanelStatusEnum.java
@@ -4,8 +4,8 @@ public enum PanelStatusEnum {
 	SCHEDULED, // flight scheduled to depart or arrive at a specific time
 	ON_TIME, // airplane currently expected to flight at its scheduled time
 	DELAYED, // airplane behind schedule and its arrival/departure time has been delayed
+	BOARDING, // passengers boarding the airplane
 	DEBOARDING, // passengers exiting the airplane
-	BOARDING, // opened boarding the airplane
 	CLOSED, // boarding closed
 	IN_TRANSIT, // currently in route to destination
 	CANCELLED, // flight cancelled and airplane not used for the scheduled flight
