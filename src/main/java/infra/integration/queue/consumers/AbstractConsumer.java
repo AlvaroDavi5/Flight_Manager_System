bb90c9990cdb15d2255958ba389ddec18ebeb050
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2023-05-06 11:16:05 -0300

    refactor: used inheritance to extends consumer classes

---
+++ src/main/java/infra/integration/queue/consumers/AbstractConsumer.java
@@ -0,0 +1,46 @@
+package infra.integration.queue.consumers;
+
+import java.util.HashMap;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import infra.integration.kafka.KafkaAdminClient;
+import infra.integration.kafka.KafkaAdminClient.ConsumerHandler;
+import app.utils.ParserUtils;
+
+public abstract class AbstractConsumer extends Thread {
+	private String topicName;
+	private KafkaAdminClient kafkaClient;
+	private KafkaConsumer<String, String> consumer;
+
+	public AbstractConsumer(KafkaAdminClient kafkaClient, String consumerGroup, String topicName) {
+		this.kafkaClient = kafkaClient;
+		this.topicName = topicName;
+		this.consumer = this.kafkaClient.createConsumer(
+				this.kafkaClient.getConsumersProperties(),
+				consumerGroup);
+		this.kafkaClient.subscribe(this.consumer, this.topicName);
+	}
+
+	public KafkaConsumer<String, String> getConsumer() {
+		return this.consumer;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			ConsumerHandler handler = (record) -> this.handleMessage(record);
+			this.kafkaClient.runPolling(this.consumer, handler);
+		}
+	}
+
+	public void handleMessage(ConsumerRecord<String, String> record) {
+		ParserUtils parser = new ParserUtils();
+
+		String key = record.key();
+		HashMap<String, Object> value = parser.stringfiedJsonToHashMap(record.value());
+
+		System.out.println(
+				"Message Key: " + key +
+						"\nMessage Value: " + parser.hashMapToStringfiedJson(value, true));
+	}
+}
--- src/main/java/infra/integration/queue/consumers/TowerReportsConsumer.java
+++ src/main/java/infra/integration/queue/consumers/TowerReportsConsumer.java
@@ -1,51 +1,24 @@
 package infra.integration.queue.consumers;
 
 import java.util.HashMap;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import infra.integration.kafka.KafkaAdminClient;
-import infra.integration.kafka.KafkaAdminClient.ConsumerHandler;
 import app.utils.ParserUtils;
 import app.services.FlightManagerService;
 
-public class TowerReportsConsumer extends Thread {
-	private String towerReportsTopic;
-	private KafkaAdminClient kafkaClient;
-	private KafkaConsumer<String, String> consumer;
+public class TowerReportsConsumer extends AbstractConsumer {
 	private FlightManagerService flightManagerService;
 
 	public TowerReportsConsumer(KafkaAdminClient kafkaClient, FlightManagerService flightManagerService) {
-		this.kafkaClient = kafkaClient;
+		super(kafkaClient, "TowerReportsConsumerGroup", System.getenv("TOWER_REPORTS_TOPIC"));
 		this.flightManagerService = flightManagerService;
-		this.towerReportsTopic = System.getenv("TOWER_REPORTS_TOPIC");
-
-		this.consumer = this.kafkaClient.createConsumer(
-				this.kafkaClient.getConsumersProperties(),
-				"TowerReportsConsumerGroup");
-		this.kafkaClient.subscribe(this.consumer, this.towerReportsTopic);
-	}
-
-	public KafkaConsumer<String, String> getConsumer() {
-		return this.consumer;
 	}
 
 	@Override
-	public void run() {
-		while (true) {
-			ConsumerHandler handler = (record) -> this.handleMessage(record);
-			this.kafkaClient.runPolling(this.consumer, handler);
-		}
-	}
-
-	private void handleMessage(ConsumerRecord<String, String> record) {
+	public void handleMessage(ConsumerRecord<String, String> record) {
 		ParserUtils parser = new ParserUtils();
 
-		String key = record.key();
 		HashMap<String, Object> value = parser.stringfiedJsonToHashMap(record.value());
-
-		System.out.println(
-				"Message Key: " + key +
-						"\nMessage Value: " + parser.hashMapToStringfiedJson(value, true));
 
 		this.flightManagerService.handleTowerReportMessage(value);
 	}
