b4b69d280391c1aa58e38b399cccf122130a1f91
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2023-05-09 23:18:24 -0300

    ci: updated script

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -2,3 +2,4 @@
 
 clear;
 source .env && mvn clean compile exec:java -Dexec.args="./src/main/resources/producer.properties ./src/main/resources/consumer.properties";
+# mvn clean compile spring-boot:run -Dspring-boot.run.arguments="./src/main/resources/producer.properties ./src/main/resources/consumer.properties";
